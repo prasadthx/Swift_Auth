ae34e4d92672bdf6063e69773c8d9b6a48cd7516
prasad <prasadzore31@gmail.com>
2021-02-10 15:45:52 +0530

    Added

---
+++ Sources/App/Models/Galaxy.swift
@@ -0,0 +1,39 @@
+import Vapor
+import Fluent
+
+final class Galaxy: Model {
+    // Name of the table or collection.
+    static let schema = "galaxies"
+
+    // Unique identifier for this Galaxy.
+    @ID(key: .id)
+    var id: UUID?
+
+    // The Galaxy's name.
+    @Field(key: "name")
+    var name: String
+
+    // Creates a new, empty Galaxy.
+    init() { }
+
+    // Creates a new Galaxy with all properties set.
+    init(id: UUID? = nil, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
+
+struct CreateGalaxy: Migration {
+    // Prepares the database for storing Galaxy models.
+    func prepare(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("galaxies")
+            .id()
+            .field("name", .string)
+            .create()
+    }
+
+    // Optionally reverts the changes made in the prepare method.
+    func revert(on database: Database) -> EventLoopFuture<Void> {
+        database.schema("galaxies").delete()
+    }
+}
